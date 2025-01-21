70ee13a62f3812f10ccf7df829f44bb72a9852a5
Kenny Oseleononmen <oseleonose@gmail.com>
2025-01-21 07:44:33 -0800

    ✏️ sample buffer and h file formatting

--- src/software_renderer.h
+++ src/software_renderer.h
@@ -1,9 +1,9 @@
 #ifndef CS248_SOFTWARE_RENDERER_H
 #define CS248_SOFTWARE_RENDERER_H
 
+#include <queue>
 #include <stdio.h>
 #include <vector>
-#include <queue>
 
 #ifdef USE_PTHREAD
 #define HAVE_STRUCT_TIMESPEC
@@ -11,8 +11,8 @@
 #endif
 
 #include "CS248.h"
-#include "texture.h"
 #include "svg_renderer.h"
+#include "texture.h"
 
 namespace CS248 { // CS248
 
@@ -24,288 +24,269 @@ double Initialize_Timer();
 class SoftwareRendererRef;
 
 class SoftwareRenderer : public SVGRenderer {
- public:
-
-  SoftwareRenderer( ) : sample_rate (1) { }
+public:
+  SoftwareRenderer() : sample_rate(1) {}
 
   // Free used resources
-  virtual ~SoftwareRenderer( ) { }
+  virtual ~SoftwareRenderer() {}
 
   // Draw an svg input to pixel buffer
-  virtual void draw_svg( SVG& svg ) = 0;
+  virtual void draw_svg(SVG &svg) = 0;
 
   // Set sample rate
-  virtual void set_sample_rate( size_t sample_rate ) = 0;
-  
+  virtual void set_sample_rate(size_t sample_rate) = 0;
+
   // Set pixel buffer
-  virtual void set_pixel_buffer( unsigned char* pixel_buffer,
-                                  size_t width, size_t height ) = 0;
+  virtual void set_pixel_buffer(unsigned char *pixel_buffer, size_t width,
+                                size_t height) = 0;
 
   // Clear pixel buffer
-  inline void clear_buffer() {
-    memset(pixel_buffer, 255, 4 * width * height);
-  }
+  inline void clear_buffer() { memset(pixel_buffer, 255, 4 * width * height); }
 
   // Set texture sampler
-  inline void set_tex_sampler( Sampler2D* sampler ) {
-    this->sampler = sampler;
-  }
+  inline void set_tex_sampler(Sampler2D *sampler) { this->sampler = sampler; }
 
   // Set svg to screen transformation
-  inline void set_canvas_to_screen( Matrix3x3 canvas_to_screen ) {
+  inline void set_canvas_to_screen(Matrix3x3 canvas_to_screen) {
     this->canvas_to_screen = canvas_to_screen;
   }
 
- protected:
-
+protected:
   // Sample rate (square root of samples per pixel)
   size_t sample_rate;
 
   // Pixel buffer memory location
-  unsigned char* pixel_buffer; 
+  unsigned char *pixel_buffer;
+  unsigned char *sample_buffer;
 
   // Pixel buffer dimension (in pixels)
-  size_t width; size_t height;
+  size_t width;
+  size_t height;
 
   // SVG outline bbox (in pixels)
-	Vector2D svg_bbox_top_left, svg_bbox_bottom_right;
+  Vector2D svg_bbox_top_left, svg_bbox_bottom_right;
 
   // Texture sampler being used
-  Sampler2D* sampler;
+  Sampler2D *sampler;
 
   // SVG coordinates to screen space coordinates
   Matrix3x3 canvas_to_screen;
 
 }; // class SoftwareRenderer
 
-
 class SoftwareRendererImp : public SoftwareRenderer {
 public:
+  SoftwareRendererImp(SoftwareRendererRef *ref = NULL)
+      : SoftwareRenderer(), ref(ref) {}
 
-	SoftwareRendererImp(SoftwareRendererRef *ref = NULL) : SoftwareRenderer(), ref(ref) { }
+  // draw an svg input to pixel buffer
+  void draw_svg(SVG &svg);
 
-	// draw an svg input to pixel buffer
-	void draw_svg(SVG& svg);
+  // set sample rate
+  void set_sample_rate(size_t sample_rate);
 
-	// set sample rate
-	void set_sample_rate(size_t sample_rate);
+  // set pixel buffer
+  void set_pixel_buffer(unsigned char *pixel_buffer, size_t width,
+                        size_t height);
 
-	// set pixel buffer
-	void set_pixel_buffer(unsigned char* pixel_buffer,
-		size_t width, size_t height);
-
-	void fill_sample(int sx, int sy, const Color& color);
-	void fill_pixel(int x, int y, const Color& color);
+  void fill_sample(int sx, int sy, const Color &color);
+  void fill_pixel(int x, int y, const Color &color);
 
 private:
+  // Primitive Drawing //
 
-	// Primitive Drawing //
+  // Draws an SVG element
+  void draw_element(SVGElement *element);
 
-	// Draws an SVG element
-	void draw_element(SVGElement* element);
+  // Draws a point
+  void draw_point(Point &p);
 
-	// Draws a point
-	void draw_point(Point& p);
+  // Draw a line
+  void draw_line(Line &line);
 
-	// Draw a line
-	void draw_line(Line& line);
+  // Draw a polyline
+  void draw_polyline(Polyline &polyline);
 
-	// Draw a polyline
-	void draw_polyline(Polyline& polyline);
+  // Draw a rectangle
+  void draw_rect(Rect &rect);
 
-	// Draw a rectangle
-	void draw_rect(Rect& rect);
+  // Draw a polygon
+  void draw_polygon(Polygon &polygon);
 
-	// Draw a polygon
-	void draw_polygon(Polygon& polygon);
+  // Draw a ellipse
+  void draw_ellipse(Ellipse &ellipse);
 
-	// Draw a ellipse
-	void draw_ellipse(Ellipse& ellipse);
+  // Draws a bitmap image
+  void draw_image(Image &image);
 
-	// Draws a bitmap image
-	void draw_image(Image& image);
+  // Draw a group
+  void draw_group(Group &group);
 
-	// Draw a group
-	void draw_group(Group& group);
+  // Rasterization //
 
-	// Rasterization //
+  // rasterize a point
+  void rasterize_point(float x, float y, Color color);
 
-	// rasterize a point
-	void rasterize_point(float x, float y, Color color);
+  // rasterize a line
+  void rasterize_line(float x0, float y0, float x1, float y1, Color color);
 
-	// rasterize a line
-	void rasterize_line(float x0, float y0,
-		float x1, float y1,
-		Color color);
+  // rasterize a triangle
+  void rasterize_triangle(float x0, float y0, float x1, float y1, float x2,
+                          float y2, Color color);
 
-	// rasterize a triangle
-	void rasterize_triangle(float x0, float y0,
-		float x1, float y1,
-		float x2, float y2,
-		Color color);
+  // rasterize an image
+  void rasterize_image(float x0, float y0, float x1, float y1, Texture &tex);
 
-	// rasterize an image
-	void rasterize_image(float x0, float y0,
-		float x1, float y1,
-		Texture& tex);
+  // resolve samples to pixel buffer
+  void resolve(void);
 
-	// resolve samples to pixel buffer
-	void resolve(void);
+  // task5 alpha compositing
+  Color alpha_blending(Color pixel_color, Color color);
 
-	// task5 alpha compositing
-	Color alpha_blending(Color pixel_color, Color color);
-
-	SoftwareRendererRef *ref;
+  SoftwareRendererRef *ref;
 }; // class SoftwareRendererImp
-
 
 class SoftwareRendererRef : public SoftwareRenderer {
 public:
+  SoftwareRendererRef(int thread_count);
 
-	SoftwareRendererRef(int thread_count);
+  virtual ~SoftwareRendererRef();
 
-	virtual ~SoftwareRendererRef();
-
-	// draw an svg input to pixel buffer
-	struct THREAD_DATA
-	{
-		SoftwareRendererRef* ref;
-		SVG* svg;
-		int x0, y0;
-		int x1, y1;
-		Matrix3x3 transformation;
-		int tid;
-	};
+  // draw an svg input to pixel buffer
+  struct THREAD_DATA {
+    SoftwareRendererRef *ref;
+    SVG *svg;
+    int x0, y0;
+    int x1, y1;
+    Matrix3x3 transformation;
+    int tid;
+  };
 
 #ifdef USE_PTHREAD
-	std::queue<int> queue;
+  std::queue<int> queue;
 
-	static void* threaded_run(void* data)
-	{
-		THREAD_DATA* thread_data = (THREAD_DATA*)data;
-		SoftwareRendererRef* ref = thread_data->ref;
-		while (1) {
-			pthread_mutex_lock(&ref->queue_lock);
-			while (ref->queue.empty()) {
-				ref->active_threads--;
-				if (ref->active_threads == 0) pthread_cond_signal(&ref->done_condition);
-				ref->inactive_threads++;
-				pthread_cond_wait(&ref->todo_condition, &ref->queue_lock);
-				ref->active_threads++;
-				ref->inactive_threads--;
-			}
+  static void *threaded_run(void *data) {
+    THREAD_DATA *thread_data = (THREAD_DATA *)data;
+    SoftwareRendererRef *ref = thread_data->ref;
+    while (1) {
+      pthread_mutex_lock(&ref->queue_lock);
+      while (ref->queue.empty()) {
+        ref->active_threads--;
+        if (ref->active_threads == 0)
+          pthread_cond_signal(&ref->done_condition);
+        ref->inactive_threads++;
+        pthread_cond_wait(&ref->todo_condition, &ref->queue_lock);
+        ref->active_threads++;
+        ref->inactive_threads--;
+      }
 
-			int id = ref->queue.front();
-			ref->queue.pop();
-			pthread_mutex_unlock(&ref->queue_lock);
-			if (id == -1) pthread_exit(0);
-			else ref->draw_svg_threaded(&ref->thread_data[id]);
-		}
-		return 0;
-	}
+      int id = ref->queue.front();
+      ref->queue.pop();
+      pthread_mutex_unlock(&ref->queue_lock);
+      if (id == -1)
+        pthread_exit(0);
+      else
+        ref->draw_svg_threaded(&ref->thread_data[id]);
+    }
+    return 0;
+  }
 
-	void draw_svg_threaded(THREAD_DATA* thread_data);
+  void draw_svg_threaded(THREAD_DATA *thread_data);
 #endif
 
-	void draw_svg(SVG& svg);
+  void draw_svg(SVG &svg);
 
-	// Clear pixel buffer
-	inline void clear_buffer();
+  // Clear pixel buffer
+  inline void clear_buffer();
 
-	// set sample rate
-	void set_sample_rate(size_t sample_rate);
+  // set sample rate
+  void set_sample_rate(size_t sample_rate);
 
-	// set pixel buffer
-	void set_pixel_buffer(unsigned char* pixel_buffer,
-		size_t width, size_t height);
+  // set pixel buffer
+  void set_pixel_buffer(unsigned char *pixel_buffer, size_t width,
+                        size_t height);
 
-	// Exposing/reimplementing these functions for CS248
+  // Exposing/reimplementing these functions for CS248
 
-	// alpha blending helper
-	Color alpha_blending_helper(Color color1, Color color2);
+  // alpha blending helper
+  Color alpha_blending_helper(Color color1, Color color2);
 
-	// rasterize a line
-	void rasterize_line_helper(float x0, float y0,
-		float x1, float y1, int width, int height,
-		Color color, SoftwareRendererImp* imp);
+  // rasterize a line
+  void rasterize_line_helper(float x0, float y0, float x1, float y1, int width,
+                             int height, Color color, SoftwareRendererImp *imp);
 
 private:
+  // Primitive Drawing //
 
-	// Primitive Drawing //
+  // Draws an SVG element
+  void draw_element(SVGElement *element, void *thread_data = NULL);
 
-	// Draws an SVG element
-	void draw_element(SVGElement* element, void* thread_data = NULL);
+  // Draws a point
+  void draw_point(Point &p, void *thread_data = NULL);
 
-	// Draws a point
-	void draw_point(Point& p, void* thread_data = NULL);
+  // Draw a line
+  void draw_line(Line &line, void *thread_data = NULL);
 
-	// Draw a line
-	void draw_line(Line& line, void* thread_data = NULL);
+  // Draw a polyline
+  void draw_polyline(Polyline &polyline, void *thread_data = NULL);
 
-	// Draw a polyline
-	void draw_polyline(Polyline& polyline, void* thread_data = NULL);
+  // Draw a rectangle
+  void draw_rect(Rect &rect, void *thread_data = NULL);
 
-	// Draw a rectangle
-	void draw_rect(Rect& rect, void* thread_data = NULL);
+  // Draw a polygon
+  void draw_polygon(Polygon &polygon, void *thread_data = NULL);
 
-	// Draw a polygon
-	void draw_polygon(Polygon& polygon, void* thread_data = NULL);
+  // Draw a ellipse
+  void draw_ellipse(Ellipse &ellipse, void *thread_data = NULL);
 
-	// Draw a ellipse
-	void draw_ellipse(Ellipse& ellipse, void* thread_data = NULL);
+  // Draws a bitmap image
+  void draw_image(Image &image, void *thread_data = NULL);
 
-	// Draws a bitmap image
-	void draw_image(Image& image, void* thread_data = NULL);
+  // Draw a group
+  void draw_group(Group &group, void *thread_data = NULL);
 
-	// Draw a group
-	void draw_group(Group& group, void* thread_data = NULL);
+  // Rasterization //
 
-	// Rasterization //
+  // rasterize a point
+  void rasterize_point(float x, float y, Color color, void *thread_data = NULL);
 
-	// rasterize a point
-	void rasterize_point(float x, float y, Color color, void* thread_data = NULL);
+  // rasterize a line
+  void rasterize_line(float x0, float y0, float x1, float y1, Color color,
+                      void *thread_data = NULL);
 
-	// rasterize a line
-	void rasterize_line(float x0, float y0,
-		float x1, float y1,
-		Color color, void* thread_data = NULL);
+  // rasterize a triangle
+  void rasterize_triangle(float x0, float y0, float x1, float y1, float x2,
+                          float y2, Color color, void *thread_data = NULL);
 
-	// rasterize a triangle
-	void rasterize_triangle(float x0, float y0,
-		float x1, float y1,
-		float x2, float y2,
-		Color color, void* thread_data = NULL);
+  void rasterize_ellipse(double cx, double cy, double rx, double ry,
+                         Color color, void *thread_data = NULL);
 
-	void rasterize_ellipse(double cx, double cy,
-		double rx, double ry,
-		Color color, void* thread_data = NULL);
+  // rasterize an image
+  void rasterize_image(Image &image);
 
-	// rasterize an image
-	void rasterize_image(Image& image);
+  // resolve samples to pixel buffer
+  void resolve(void);
 
-	// resolve samples to pixel buffer
-	void resolve(void);
+  // Helpers //
+  void fill_sample(float sx, float sy, const Color &c,
+                   void *thread_data = NULL);
+  void fill_pixel(int x, int y, const Color &c, void *thread_data = NULL);
 
-	// Helpers //
-	void fill_sample(float sx, float sy, const Color& c, void* thread_data = NULL);
-	void fill_pixel(int x, int y, const Color& c, void* thread_data = NULL);
-
-	// SSAA sample buffer
-	std::vector<unsigned char> sample_buffer;
+  // SSAA sample buffer
+  std::vector<unsigned char> sample_buffer;
 
 #ifdef USE_PTHREAD
-	std::vector<pthread_t> threads;
-	pthread_attr_t attr;
-	pthread_mutex_t queue_lock;
-	pthread_cond_t done_condition, todo_condition;
-	int active_threads, inactive_threads;
-	int thread_count;
-	int sqrt_thread_count;
-	std::vector<THREAD_DATA> thread_data;
+  std::vector<pthread_t> threads;
+  pthread_attr_t attr;
+  pthread_mutex_t queue_lock;
+  pthread_cond_t done_condition, todo_condition;
+  int active_threads, inactive_threads;
+  int thread_count;
+  int sqrt_thread_count;
+  std::vector<THREAD_DATA> thread_data;
 #endif
 
 }; // class SoftwareRendererRef
-
 
 } // namespace CS248
 
