8df45a4916b314c5ee5daa625bab4b1aa98a8a8a
bridgetepatrick <116386208+bridgetepatrick@users.noreply.github.com>
2025-01-23 12:47:18 -0800

    ✨ implement normalized device coordinate transformation for SVG canvas

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -8,12 +8,24 @@ void ViewportImp::set_viewbox( float x, float y, float span ) {
 
   // Task 3 (part 2): 
   // Set svg to normalized device coordinate transformation. 
-  // Your input arguments are defined as SVG canvans coordinates.
-
+  // Your input arguments are defined as SVG canvas coordinates.
   this->x = x;
   this->y = y;
   this->span = span; 
+    // Scale
+  float scale_x = 1.0f / (2*span);
+  float scale_y = 1.0f / (2*span);
+    // Translate to align the top-left of the viewport to (0, 0)
+  float trans_x = -(x - span);
+  float trans_y = -(y - span);
 
+  Matrix3x3 canvas_to_norm = Matrix3x3::identity();
+  canvas_to_norm[0][0] = scale_x;                   
+  canvas_to_norm[1][1] = scale_y;                  
+  canvas_to_norm[2][0] = scale_x * trans_x;     
+  canvas_to_norm[2][1] = scale_y * trans_y;     
+
+  svg_2_norm = canvas_to_norm;
 }
 
 void ViewportImp::update_viewbox( float dx, float dy, float scale ) { 
